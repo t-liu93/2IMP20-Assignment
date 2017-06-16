3156ffa0c0c3d4413204fb4f5b22de5bb99d2baa
Tianyu Liu <t.liu.1@student.tue.nl>
2017-06-16 10:49:13 +0100

    # Make import changes in Xtext2Ecore to solve the exceptions

---
+++ GltAssignment2/RobotDSL/robotDSL/src/glt/Xtext2Ecore.java
@@ -0,0 +1,49 @@
+package glt;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import glt.serializer.RobotDSLSemanticSequencer;
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.ISetup;
+import org.eclipse.xtext.common.TerminalsStandaloneSetup;
+import org.eclipse.xtext.resource.IResourceFactory;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
+import org.eclipse.xtext.resource.XtextResourceSet;
+
+public class Xtext2Ecore {
+	private static void exportXMI(String absuloteTargetFolderPath, String fileBaseName) {
+	   		new RobotDSLStandaloneSetup().createInjectorAndDoEMFRegistration();
+	   		
+	   		new robotDSLEcore.util.RobotDSLEcoreAdapterFactory();
+	
+	    String inputURI = "file:///" + absuloteTargetFolderPath + "/" + fileBaseName + ".rbdsl";
+	    String outputURI = "file:///" + absuloteTargetFolderPath + "/" + fileBaseName + ".xmi";
+	    
+	    ResourceSet rs = new ResourceSetImpl();
+	    Resource resource = rs.getResource(URI.createURI(inputURI), true);	    
+
+	    EcoreUtil.resolveAll(resource);
+
+	    Resource xmiResource = rs
+	            .createResource(URI.createURI(outputURI));
+	    xmiResource.getContents().add(resource.getContents().get(0));
+	    try {
+	        xmiResource.save(null);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	public static void main(String[] args){
+		String folder = "/home/osboxes/Desktop/workspace/2IMP20-Assignment/GltAssignment2/SupportedFileForA3/SampleInputAndGeneratedClass";
+		exportXMI(folder, "SampleBuildStep1");
+	}
+}
