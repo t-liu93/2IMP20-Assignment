1d506b4b030a50cf32b6cb8b29d780a58e79e905
osboxes <osboxes@osboxes>
2017-06-16 14:03:27 +0100

    # for query

---
+++ GltAssignment2/Robot2Java/src/App.java
@@ -0,0 +1,16 @@
+
+import java.io.File;
+
+import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		EgxModule module = new EgxModule(new EglFileGeneratingTemplateFactory());
+
+	}
+
+}
